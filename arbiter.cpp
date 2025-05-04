0e2d1424dcfd44c799b8e75c799ec81276366eb4
知念夏世 <chart11from21@outlook.com>
2025-05-04 13:33:50 +0800

    feature: auto set arbiter time to current time

--- arbiter.cpp
+++ arbiter.cpp
@@ -6,8 +6,8 @@ arbiter::arbiter(QWidget *parent)
     , ui(new Ui::arbiter)
 {
     ui->setupUi(this);
-
-
+    ui->dateEdit->setDisplayFormat("yyyy-MM-dd");
+    ui->dateEdit->setDate(QDate::currentDate());
 }
 
 void arbiter::closeEvent(QCloseEvent *event) {
